85f0bc5eb96aac2f4cda8f97add7ef7c421bd581
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-29 15:16:58 -0300

    Criadas as funções para mostrar todas as categorias e seus respectivos produtos

--- services/produtos-services.js
+++ services/produtos-services.js
@@ -20,6 +20,57 @@ function mostraTodosProdutos() {
     })
 }
 
+function mostraProdutosCategorizados() {
+    fetch("http://localhost:3000/categorias")
+    .then(response => {
+        return response.json();
+    })
+    .then(categorias => {
+        const produtosContainer = document.querySelector("[data-produtos]");
+        const produtosCategorias = Object.entries(categorias);
+
+        produtosCategorias.forEach(categoria => {
+            produtosContainer.innerHTML += 
+            `
+            <div id="${categoria[0]}" class="produtos__categoria" data-categoria="${categoria[1]}">
+                <div class="produtos__cabecalho">
+                    <h2 class="produtos__titulo">${categoria[1]}</h2>
+                    <a href="produtos-home.html" class="produtos__subtitulo">Ver tudo &#10140;</a>
+                </div>
+                <div class="produtos__container">
+
+                </div>
+            </div>
+            `
+
+            mostraProdutosEmCategorias(categoria[1]);
+        })
+    })
+}
+
+function mostraProdutosEmCategorias(produtosCategoria) {
+    fetch(`http://localhost:3000/produtos?categoria=${produtosCategoria}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(produtos => {
+        const containerCategoria = document.querySelector(`[data-categoria="${produtosCategoria}"]`);
+
+        produtos.forEach(produto => {
+            containerCategoria.lastElementChild.innerHTML +=
+            `
+            <div class="produto">
+                <img class="produto__imagem" src="${produto.imagem}" alt="">
+                <h3 class="produto__nome">${produto.nome}</h3>
+                <h4 class="produto__preco">${produto.preco}</h4>
+                <a class="produto__link" href="produto-selecionado.html?id=${produto.id}">Ver produto</a>
+            </div>
+            `
+        })
+    })
+}
+
 export const produtosServices = {
-    mostraTodosProdutos
+    mostraTodosProdutos,
+    mostraProdutosCategorizados
 }
