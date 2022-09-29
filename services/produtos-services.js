7ddf2f97dd31d541c6dd496a993457e0c054c8e3
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-29 20:05:15 -0300

    Funções para mostrar produto individual e similares adicionadas, melhoria em algumas outras funções

--- services/produtos-services.js
+++ services/produtos-services.js
@@ -43,34 +43,74 @@ function mostraProdutosCategorizados() {
             </div>
             `
 
-            mostraProdutosEmCategorias(categoria[1]);
+            colocaProdutosEmCategorias(categoria[1]);
         })
     })
 }
 
-function mostraProdutosEmCategorias(produtosCategoria) {
+function mostraProdutos(produtos, containerProdutos) {
+    produtos.forEach(produto => {
+        containerProdutos.innerHTML +=
+        `
+        <div class="produto">
+            <img class="produto__imagem" src="${produto.imagem}" alt="">
+            <h3 class="produto__nome">${produto.nome}</h3>
+            <h4 class="produto__preco">${produto.preco}</h4>
+            <a class="produto__link" href="produto-selecionado.html?id=${produto.id}&categoria=${produto.categoria}">Ver produto</a>
+        </div>
+        `
+    })
+}
+
+function colocaProdutosEmCategorias(produtosCategoria) {
     fetch(`http://localhost:3000/produtos?categoria=${produtosCategoria}`)
     .then(response => {
         return response.json();
     })
     .then(produtos => {
-        const containerCategoria = document.querySelector(`[data-categoria="${produtosCategoria}"]`);
+        const containerCategoria = document.querySelector(`[data-categoria="${produtosCategoria}"]`).lastElementChild;
 
-        produtos.forEach(produto => {
-            containerCategoria.lastElementChild.innerHTML +=
-            `
-            <div class="produto">
-                <img class="produto__imagem" src="${produto.imagem}" alt="">
-                <h3 class="produto__nome">${produto.nome}</h3>
-                <h4 class="produto__preco">${produto.preco}</h4>
-                <a class="produto__link" href="produto-selecionado.html?id=${produto.id}">Ver produto</a>
-            </div>
-            `
-        })
+        mostraProdutos(produtos, containerCategoria);
+    })
+}
+
+function mostraProdutosSimilares(produtoMostradoID, produtosCategoria, containerCategoria) {
+    fetch(`http://localhost:3000/produtos?categoria=${produtosCategoria}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(produtos => {
+        const todosProdutos = produtos.filter((produto => {
+            return produtoMostradoID != produto.id;
+        }));
+
+        mostraProdutos(todosProdutos, containerCategoria);
+    })
+}
+
+function mostraProdutoIndividual(id) {
+    fetch(`http://localhost:3000/produtos/${id}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(produto => {
+        const containerProduto = document.querySelector("[data-produto]");
+
+        containerProduto.innerHTML +=
+        `
+        <img class="produto-selecionado__imagem" src="${produto.imagem}" alt="">
+        <div class="produto-selecionado__informacoes">
+            <h1 class="produto-selecionado__titulo">${produto.nome}</h1>
+            <h2 class="produto-selecionado__preco">${produto.preco}</h2>
+            <h3 class="produto-selecionado__descricao">${produto.descricao}</h3>
+        </div>
+        `
     })
 }
 
 export const produtosServices = {
     mostraTodosProdutos,
-    mostraProdutosCategorizados
+    mostraProdutosCategorizados,
+    mostraProdutosSimilares,
+    mostraProdutoIndividual
 }
