cd5cc11b8bf76363c144498d8ec7ab97afc8aa60
Gabriel <gabriel.gracilianomb@gmail.com>
2022-09-30 17:08:03 -0300

    Função para detectar os botões de deletar e editar, função de deletar produto sendo chamada

--- controllers/listaTodosProdutos-controller.js
+++ controllers/listaTodosProdutos-controller.js
@@ -1,3 +1,25 @@
 import { produtosServices } from "../services/produtos-services.js";
 
+const produtosContainer = document.querySelector("[data-produtos]");
+
+produtosContainer.addEventListener("click", (event) => {
+    const tipoBotao = event.target.dataset.botao;
+    const produtoContainer = event.target.parentElement.parentElement;
+    const produtoID = produtoContainer.dataset.produtoId;
+
+    if(tipoBotao) {
+        switch(tipoBotao) {
+            case "deletar":
+                produtoContainer.remove();
+                produtosServices.deletaProduto(produtoID);
+                break;
+            case "editar":
+                window.location.href = `adiciona-produtos.html?id=${produtoID}`;
+                break;
+            default:
+                throw new Error("Botão não existe!");
+        }
+    }
+});
+
 produtosServices.mostraTodosProdutos();
