910a872bbbb90c6968cc34358494b519edbbff86
Gabriel <gabriel.gracilianomb@gmail.com>
2022-10-11 21:41:02 -0300

    Campos de contato obtidos por data attributes

--- controllers/validacao-controller.js
+++ controllers/validacao-controller.js
@@ -1,6 +1,6 @@
 import { validacaoServices } from "../services/validacao-services.js";
 
-const campos = document.querySelectorAll(".formulario__campo");
+const campos = document.querySelectorAll("[data-form-contato]");
 
 campos.forEach(campo => {
     campo.addEventListener("blur", (event) => {
